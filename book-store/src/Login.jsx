3405b503092cb79b3e16dd6860a8947b48609206
Mohamed Amarcha <simoamarcha@icloud.com>
2023-04-30 02:58:44 +0100

    responsive/erros

--- book-store/src/Login.jsx
+++ book-store/src/Login.jsx
@@ -1,51 +1,52 @@
 import Navbar from "./Navbar";
 import { useState } from "react";
-import {auth} from './firebase'
+import { auth } from './firebase'
 import { signInWithEmailAndPassword } from "firebase/auth";
 import AuthDetails from './AuthDetails'
 
-export default function Login(){
-    
-    const [email,setEmail] = useState('');
-    const [password,setPassword] = useState('');
+export default function Login() {
 
-    const SignIn = (e) => {
-        e.preventDefault();
-        signInWithEmailAndPassword(auth, email, password)
-          .then((userCredential) => {
-            console.log(userCredential);
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      };
-      
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-    return(
-        <div>
-        <form className="signup" onSubmit={SignIn}>
+  const SignIn = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        setError(error.message.replace('Firebase: ', ''));
+      });
+  };
+
+  return (
+    <div>
+      <form className="signup" onSubmit={SignIn}>
         <div className="formm">
-            <label className="labell" >Email:</label>
-            <input
-                className="inputt"
-                type="email"
-                placeholder="Enter your email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                />
-              <label className="labell" >Password:</label>
+          <label className="labell">Email:</label>
+          <input
+            className="inputt"
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label className="labell">Password:</label>
 
-                <input
-                className="inputt"
-                type="password"
-                placeholder="Enter your password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                />
-            <button className="button-39 btn" type="submit" >login</button>
-            </div>
-        </form>
-        {/* <AuthDetails /> */}
+          <input
+            className="inputt"
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="button-39 btn" type="submit">login</button>
         </div>
-    )
+      </form>
+      {error && <p>{error}</p>}
+      {/* <AuthDetails /> */}
+    </div>
+  )
 }
