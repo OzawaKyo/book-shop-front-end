d83edbc17095ff16ab62e2c09ccd43682e12fc78
Mohamed Amarcha <simoamarcha@icloud.com>
2023-04-30 16:49:06 +0100

    dislay name

--- book-store/src/Login.jsx
+++ book-store/src/Login.jsx
@@ -1,8 +1,10 @@
 import Navbar from "./Navbar";
 import { useState } from "react";
-import { auth } from './firebase'
+import { auth , provider } from "./firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import AuthDetails from './AuthDetails'
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
 
 export default function Login() {
 
@@ -20,6 +22,27 @@ export default function Login() {
         setError(error.message.replace('Firebase: ', ''));
       });
   };
+
+      const handleSignIn =()=>{
+        signInWithPopup(auth, provider)
+    .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+    }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+    });}
 
   return (
     <div>
@@ -44,6 +67,7 @@ export default function Login() {
           />
           <button className="button-39 btn" type="submit">login</button>
         </div>
+        <button className="button-39 btn" onClick={handleSignIn}>Log in with Google</button>
       </form>
       {error && <p>{error}</p>}
       {/* <AuthDetails /> */}
--- book-store/src/SignUp.jsx
+++ book-store/src/SignUp.jsx
@@ -1,31 +1,85 @@
 import Navbar from "./Navbar";
 import { useState } from "react";
-import {auth} from './firebase'
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth , provider } from "./firebase";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import './SignUp.css'
+
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 export default function Login(){
     
     const [email,setEmail] = useState('');
     const [password,setPassword] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
 
     const SignUp = (e) => {
         e.preventDefault();
         createUserWithEmailAndPassword(auth, email, password)
           .then((userCredential) => {
             console.log(userCredential);
+            // Update the user's profile with their first name and last name
+            updateProfile(userCredential.user, {
+              displayName: `${firstName} ${lastName}`,
+            });
           })
           .catch((error) => {
             console.log(error);
           });
       };
-      
+    
+    const handleSignIn =()=>{
+        signInWithPopup(auth, provider)
+          .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+            // IdP data available using getAdditionalUserInfo(result)
+            // ...
+          })
+          .catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+          });
+    }
 
     return(
         <div>
         <form className="signup" onSubmit={SignUp}>
             
             <div className="formm">
+              <div className="smiaaa">
+                <div className="kniaaa">
+                  <label className="labell" >First Name:</label>
+                  <input
+                      className="inputt i"
+                      type="text"
+                      placeholder="Enter your first name"
+                      value={firstName}
+                      onChange={(e) => setFirstName(e.target.value)}
+                  />
+                </div>
+                <div className="kniaaa">
+                  <label className="labell" >Last Name:</label>
+                  <input
+                      className="inputt i"
+                      type="text"
+                      placeholder="Enter your last name"
+                      value={lastName}
+                      onChange={(e) => setLastName(e.target.value)}
+                  />
+                </div>
+                
+              </div>
+              
               <label className="labell" >Email:</label>
               <input
                   className="inputt"
@@ -45,6 +99,8 @@ export default function Login(){
               <button className="button-39 btn" type="submit" >Sign up</button>
   
             </div>
+              <button className="button-39 btn" onClick={handleSignIn}>Sign Up in with Google</button>
+
          </form>
         </div>
     )
